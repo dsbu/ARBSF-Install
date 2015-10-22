d0629300fdd38019f624c6febcd18c73944392d4
Douglas Speckhard <dsbu@iMac.local>
2015-10-21 22:12:38 -0700

    Initial Commit

---
+++ app-install.sh
@@ -0,0 +1,33 @@
+# Apps List
+apps=(
+# Utilities
+  alfred
+  bartender
+  atom
+  cyberduck
+  crashplan
+  harvest
+# Browsers
+  google-chrome
+  firefox
+#Productivity
+  microsoft-office
+  adobe-air
+  adobe-reader
+  adobe-creative-cloud
+  skim
+# cloud drives
+  google-drive
+  box-sync
+  dropbox
+# Communications
+  zoomus
+)
+
+#Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
