b2029ff6dec36152b48f12212b92a42031420b84
Doug Speckhard <doug.speckhard@gmail.com>
2015-10-21 23:29:26 -0700

    Create dock-setup.sh

---
+++ dock-setup.sh
@@ -0,0 +1,11 @@
+  ./dockutil --add /Applications/Microsoft\ Word.app --after Keynote
+  ./dockutil --add /Applications/Microsoft\ Excel.app  --after Microsoft Word
+  ./dockutil --add /Applications/Microsoft\ Excel.app  --after "Microsoft Word"
+  ./dockutil --add /Applications/Microsoft\ PowerPoint.app  --after "Microsoft Excel"
+  ./dockutil --add /Applications/Microsoft\ OneNote.app  --after "Microsoft Powerpoint"
+  ./dockutil --add /Applications/zoom.us.app
+  ./dockutil --add /Applications/Google\ Chrome.app --after Safari
+  ./dockutil --add /Applications/Firefox.app --after "Google Chrome"
+  ./dockutil --add /Applications/Adobe\ Acrobat\ Reader\ DC.app --after "Microsoft OneNote"
+  ./dockutil --add /Applications/Skim.app --after "Adobe Acrobat Reader DC"
+  
