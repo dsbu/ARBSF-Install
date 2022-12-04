ab2422ea02095efd4228e2763beb84cdcd1517fd
Doug Speckhard <doug.speckhard@gmai.com>
2022-12-04 10:55:56 -0800

    updated app-install script

--- app-install.sh
+++ app-install.sh
@@ -2,25 +2,29 @@
 apps=(
 # Utilities
   alfred
+  backblaze
   #bartender - maybe don't need this
   #atom
   cyberduck
+  displaylink
+  flux
   #crashplan
-  harvest
+  # harvest
 # Browsers
   google-chrome
   firefox
+  rectangle
 #Productivity
   #microsoft-office
-  adobe-air
+  # adobe-air
   adobe-acrobat-reader
   #adobe-creative-cloud
   #skim
 # cloud drives
 #  google-drive
   google-drive-file-stream
-  box-sync
-  dropbox
+  # box-sync
+  # dropbox
 # Communications
   zoomus
 )
@@ -31,7 +35,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 #install cask
 # Seems out of date as of 7/5/18
 # brew install caskroom/cask/brew-cask
-
+g
 # Install apps to /Applications
 # Default is: /Users/$user/Applications
 echo "installing apps..."
